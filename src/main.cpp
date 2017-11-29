3e066171ea717f97b2946581e46591185b132db3
Ariyan.Wasi <kurdish_yoda@Ariyan.Wasi-HP-Notebook>
2017-11-29 10:55:06 +0100

    Comented

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #define glavnaSerial_Baud 9600
 #define Dynamixel_Baud    1000000
 
-char buffer[8];
+char buffer[8];                   // buffer length
 bool buffer_read = false;
 
 char tool_id; int item_id; char function_id; unsigned char value1; unsigned char value2;
@@ -20,13 +20,13 @@ bool debug = false;
 
 void setupz() {
 
+/*Initializing Serial or Dynamixel comunication*/
   if(debug) {
     Serial.begin(Serial_Baud);
   } else {
-    Dynamixel.begin(Dynamixel_Baud, DIRECTION_PIN);
+     Dynamixel.begin(Dynamixel_Baud, DIRECTION_PIN);
   }
-
-  glavna_serial.begin(glavnaSerial_Baud);
+    glavna_serial.begin(glavnaSerial_Baud);
 
 }
 
@@ -34,62 +34,57 @@ void setup() {
   setupz();
 }
 
-void loop() {
 
+
+void loop() {
+// If connection whit "glavna" is established, get data (bytes)
   if(glavna_serial.available() >= 8) {
     glavna_serial.readBytes(buffer, 8);
     buffer_read = true;
   }
-
+// Assinging values to the buffers
   if(buffer_read) {
     tool_id     = buffer[0];
     item_id     = buffer[1];
     function_id = buffer[2];
     value1      = buffer[3];
     value2      = buffer[4];
-
+// Print the gotten buffers if debuging
     if(debug) { Serial.println(tool_id); Serial.println(item_id); Serial.println(function_id); Serial.println(value1); Serial.println(value2); Serial.println("-----------------------"); }
-
+// If not run servos
     if(!debug) {
-      if(tool_id == 'a') {
-        if(function_id == 'm') {
-          int position = (int) 1024 / (255 / value1);
-          int speed    = (int) 1024 / (255 / value2);
+      if(tool_id == 'a') {                                //If Dynamixel is Initialized
+        if(function_id == 'm') {                          // start "move" protocol
+          int position = (int) 1024 / (255 / value1);     //   Coverting into position
+          int speed    = (int) 1024 / (255 / value2);     //   and speed
 
-          Dynamixel.moveSpeed(item_id, position, speed);
+          Dynamixel.moveSpeed(item_id, position, speed);  //Initialize Servo
 
-          glavna_serial.write(1);
+          glavna_serial.write(1);                         //return to "glavna"
 
-        } else if(function_id == 'l') {
+        } else if(function_id == 'l') {                   // running led-status
           if(value1 == 1) {
             Dynamixel.ledStatus(item_id, true);
           } else {
             Dynamixel.ledStatus(item_id, false);
           }
 
-          glavna_serial.write(1);
-        } else if(function_id == 'M') {
+          glavna_serial.write(1);                        // return to "glavna"
+        } else if(function_id == 'M') {                  //running "movement" check
             int moving;
-            moving = Dynamixel.moving(item_id);
+            moving = Dynamixel.moving(item_id);          //getting status of "movement"
 
-            glavna_serial.write(1);
+            glavna_serial.write(1);                      //retrun to "glavna"
 
-            glavna_serial.write((unsigned char)moving);
+            glavna_serial.write((unsigned char)moving);  // return movement status to "glavna"
 
-        } else if (function_id == 'p') {
-          int position, calculated_pos;
-          position = Dynamixel.readPosition(item_id);
 
-          glavna_serial.write(1);
-
-          calculated_pos = ((255 / (1024 / position)) * 300) / 255;
-
-          glavna_serial.write((unsigned char)calculated_pos);
         }
 
       }
 
     } else {
+/* print position and speed goten from "glavna" if debug is Initialized */
 
       int position = (int) 1024 / (255 / value1);
       int speed    = (int) 1024 / (255 / value2);
@@ -100,6 +95,6 @@ void loop() {
 
     }
 
-    buffer_read = false;
+    buffer_read = false;                               //closeing down buffer handleing
   }
 }
